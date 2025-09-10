8956047b37d506167b652acd9cd8ff583b127456
Bruno Crivelari Sanches <bcsanches@gmail.com>
2025-09-10 14:28:38 -0300

    first mod! weee

---
+++ mod/compile.cmd
@@ -0,0 +1,4 @@
+d:\games\QuakeEdit\ftqcc\fteqcc64.exe progs\progs.src
+
+move progs.dat d:\games\vkQuake\mymod\
+del progs.lno
---
+++ mod/run.cmd
@@ -0,0 +1,6 @@
+pushd
+
+cd \games\vkQuake\
+vkquake.exe -game mymod +map bcs_01
+
+popd
